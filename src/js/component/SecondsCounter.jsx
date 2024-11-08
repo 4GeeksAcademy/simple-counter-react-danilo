1dee033e6fe1b456622554886bbb06b231f4b869
DaniloYP <daniloyp@gmail.com>
2024-11-08 20:10:33 +0000

    counter terminado danilo

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -4,31 +4,31 @@ const SecondsCounter = ({seconds}) => {
     return (
         <div className="container-fluid">
             <div className="row bg-dark text-white p-5 mx-auto fa-5x">
-                <div className="col">
+                <div className="col d-flex justify-content-center align-items-center bg-dark bg-gradient border border-dark rounded mb-3 mx-3" style={{ width: '100px', height: '190px'}}>
                 <i className="fa-regular fa-clock"></i>
                 </div>
 
-                <div className="col">
+                <div className="col d-flex justify-content-center align-items-center bg-dark bg-gradient border border-dark rounded  mb-3 mx-3" style={{ width: '100px', height: '190px'}} >
                 0
                 </div>
 
-                <div className="col">
+                <div className="col d-flex justify-content-center align-items-center bg-dark bg-gradient border border-dark rounded  mb-3 mx-3" style={{ width: '100px', height: '190px'}}>
                 0
                 </div>
 
-                <div className="col">
+                <div className="col d-flex justify-content-center align-items-center bg-dark bg-gradient border border-dark rounded  mb-3 mx-3" style={{ width: '100px', height: '190px'}}>
                 0
                 </div>
 
-                <div className="col">
+                <div className="col d-flex justify-content-center align-items-center bg-dark bg-gradient border border-dark rounded  mb-3 mx-3" style={{ width: '100px', height: '190px'}}>
                 0
                 </div>
 
-                <div className="col">
+                <div className="col d-flex justify-content-center align-items-center bg-dark bg-gradient border border-dark rounded  mb-3 mx-3" style={{ width: '100px', height: '190px'}}>
                 {Math.floor(seconds/ 10) % 10}
                 </div>
 
-                <div className="col">
+                <div className="col d-flex justify-content-center align-items-center bg-dark bg-gradient border border-dark rounded  mb-3 mx-3" style={{ width: '100px', height: '190px'}}>
                 {Math.floor(seconds/ 1) % 10}
                 </div>
 
